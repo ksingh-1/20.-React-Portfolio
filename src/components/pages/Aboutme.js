3ea708adbb4ca47e9951bab34cc79d0abcf1ab83
SSVF2 <kulpreet.s18@gmail.com>
2020-10-02 13:31:32 -0500

    update about me page

--- src/components/pages/Aboutme.js
+++ src/components/pages/Aboutme.js
@@ -2,5 +2,36 @@ import React from 'react';
 import '../../App.css';
 
 export default function AboutMe() {
-  return <h1 className='aboutme'>About Me</h1>;
+  return (
+    <section id="main-info" className="clearfix">
+    <h1 className='aboutme'>
+      <p>About Me </p>
+      </h1>
+      {/* <img id="profilePic" rel="profilePic" */}
+      <div> 
+        <p>
+          Hello TEST
+        </p>
+        <br />
+        <p id="itemsAM">About Me:</p>
+          <ul>
+            <li>
+            I am a Software Developer located in Oklahoma with a background in Hospitality Management and Bacherlors Degree Psychology looking to make the change into Tech and Software Development!
+            </li>
+            <li>
+            My fondest memories in life have been about cars and creating something that is unique, special and your own. I see technology and software very close to similar as working on cars. You can create something from scratch or work with a team to improve existing code to optimize it to be better.
+            </li>
+            <li>
+            My long term goal is to work on technology and software that will be used in cars and trucks!
+            </li>
+            <li>
+            I hope to grow and expand my career in programming and web development to help people in the future!
+            </li>
+
+          </ul>
+
+      </div>
+
+   </section>
+  )
 }
