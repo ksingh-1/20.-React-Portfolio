3eb1929e04b8850b39b78d836c1acdb1e7e4efe5
SSVF2 <kulpreet.s18@gmail.com>
2020-10-01 22:27:45 -0500

    update resume.js to display my resume from google docs

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import '../../App.css';
 
-export default function Resume() {
-  return <h1 className='resume'>Resume</h1>;
-  
+// export default 
+function Resume() {
+  return (
+  <section id="main-resume" className="clearfix">
+
+    <h1 className='resume'>
+      <p>Resume</p>
+      </h1>
+      <div className="rBox">
+        <iframe title="KSingh Resume" className="cv" src="https://docs.google.com/document/d/e/2PACX-1vRocmpNFiWb5J1aTZ5_XCXCpbVT6wA0JQDkrjBx72n_tOHzmZa9kqx2GDDmKyydk-VHAdKbbWx-JYIZ/pub?embedded=true"></iframe>
+      </div>
+  </section>
+  )
 }
+
+export default Resume;
