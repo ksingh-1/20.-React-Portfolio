c609a29a0ae974cc02e3b6f940887a10fb0b90cd
SSVF2 <kulpreet.s18@gmail.com>
2020-11-06 16:41:41 -0600

    update card descriptions to lowercase first letters

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -11,7 +11,7 @@ function Cards() {
           <ul className='cards__items'>
             <CardItem
               src='images/Project1BNG.JPG'
-              text='Bubs & Grubs Is A Great Application To Try New Quarantine Meals Along With Drink Ideas Based Upon The Age Inputted When First Using The App.'
+              text='Bubs & Grubs is a great application to try out new quarantine meals along with drink ideas based on the age inputted when first using the app.'
               label='Team Project 1'
               href='https://github.com/ksingh-1/bng'
               web='https://rgarza28.github.io/bng/'
@@ -20,7 +20,7 @@ function Cards() {
             />
             <CardItem
               src='images/Project2Druid.JPG'
-              text='DRUID (Develop/Restaurant User Interface/Design) Allows Restaurant Owners/Users To Sign-Up, Sign-In Add & Remove Items From The Menu.'
+              text='DRUID (Develop/Restaurant User Interface/Design) Allows restaurant owners/workers to sign-up, sign-in add & remove items from the menu.'
               label='Team Project 2'
               href='https://github.com/KenanDeari/Digital-Menu-Project'
               web='https://mighty-gorge-52471.herokuapp.com/'
@@ -29,7 +29,7 @@ function Cards() {
             />
             <CardItem
               src='images/Project3SMU.JPG'
-              text='S.M.U. (Student Management Utility) Is A User Friendly Site Where Students/Alumni Can Apply To Jobs And Look For Networking Events.'
+              text='S.M.U. (Student Management Utility) is a user friendly site where students/alumni can apply to jobs and look for networking events.'
               label='Team Project 3'
               href='https://github.com/KenanDeari/S.M.U---Student-Managment-Utility'
               web='https://studentmanagementutility.herokuapp.com/'
@@ -40,7 +40,7 @@ function Cards() {
           <ul className='cards__items'>
             <CardItem
               src='images/WeatherApp.PNG'
-              text='Weather Dashboard Application Uses OpenWeather API To Retrieve Weather Data For Various Cities Inputted By The User. Persistent Cities Searched Will Be Saved In Local Storage On The Left.'
+              text='Weather Dashboard Application uses OpenWeather API to retrieve weather data for various cities inputted by the user. Favorite cities searched will be saved in local storage on the left.'
               label='Homework'
               href='https://github.com/ksingh-1/06-Weather-Dashboard'
               web='https://ksingh-1.github.io/06-Weather-Dashboard/'
@@ -49,7 +49,7 @@ function Cards() {
             />
             <CardItem
               src='images/TeamProfile.PNG'
-              text='Team Profile Generator Application Will Prompt The User For Information About The Team Manager And Team Members To Generate A Team Profile Page From The Command Line. No Website Deployment.'
+              text='Team Profile Generator Application prompts the iser for information about the Team Manager and Team Members to generate a aeam profile page from the command line. -No Website Deployment-'
               label='Homework'
               href='https://github.com/ksingh-1/10.Team-Profile-Generator'
               web=''
@@ -58,7 +58,7 @@ function Cards() {
             />
             <CardItem
               src='images/NoteTakerMainPage.PNG'
-              text='Note Taker Application uses An Express Backend To Save And Retrieve Various Notes Made By The User From and To A JSON File. The Note Files Can Be Editted or Deleted By The User.'
+              text='Note Taker Application uses an Express backend to save and retrieve various notes made by the user to and from a JSON file. The note files can be editted or deleted by the user.'
               label='Homework'
               href='https://github.com/ksingh-1/11.-Note-Taker'
               web='https://lit-bastion-38303.herokuapp.com/'
@@ -69,7 +69,7 @@ function Cards() {
           <ul className='cards__items'>
             <CardItem
               src='images/FitnessTracker.jpg'
-              text='Fitness Tracking App That Will Help People Reach Mheir Fitness Goals Quicker And Track Workout Progress. This App Uses MongoDB To Store Data.'
+              text='Fitness Tracking App will help people reach their fitness goals quicker and track workout progress. This app uses MongoDB to store data.'
               label='Homework'
               href='https://github.com/ksingh-1/17.-Fitness-Tracker'
               web='https://limitless-lake-38302.herokuapp.com/'
@@ -82,7 +82,7 @@ function Cards() {
             />
             <CardItem
               src='images/Day Planner App.gif'
-              text='Day Planner Is A Calendar Application That Allows The User To Save Events For Each Hour Of The Day. This Application Checks And Updates The Time As Well.'
+              text='Day Planner is a calendar application that allows the user to save events for each hour of the day. This application checks and updates the time as well.'
               label='Homework'
               href='https://github.com/ksingh-1/05-Day-Planner'
               web='https://ksingh-1.github.io/05-Day-Planner/'
@@ -91,7 +91,7 @@ function Cards() {
             />
             <CardItem
               src='images/BudgetTracker.jpg'
-              text='Budget Tracker Application Will Help People Track Their Expenses And Deposits To Be More Financially Consciencious. This App Uses MongoDB.'
+              text='Budget Tracker Application will help people track their expenses and deposits to be more financially consciencious. This app uses MongoDB.'
               label='Homework'
               href='https://github.com/ksingh-1/18.-Budget-Tracker'
               web='https://guarded-badlands-40628.herokuapp.com/'
